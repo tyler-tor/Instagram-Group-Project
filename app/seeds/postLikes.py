7f8e69230398812d28ea066cf027b05c0ca71455
tyler <dtctorrero@icloud.com>
2022-10-24 20:32:32 -0700

    added seeds for postlikes

---
+++ app/seeds/postLikes.py
@@ -0,0 +1,27 @@
+from app.models import db, PostLike
+
+def seed_post_likes():
+    postlike1 = PostLike(
+        user_id=1,
+        post_id=3
+    )
+    postlike2 = PostLike(
+        user_id=2,
+        post_id=1
+    )
+    postlike3 = PostLike(
+        user_id=3,
+        post_id=2
+    )
+
+
+    db.session.add(postlike1)
+    db.session.add(postlike2)
+    db.session.add(postlike3)
+
+    db.session.commit()
+
+
+def undo_post_likes():
+    db.session.execute('TRUNCATE post_likes RESTART IDENTITY CASCADE;')
+    db.session.commit()
