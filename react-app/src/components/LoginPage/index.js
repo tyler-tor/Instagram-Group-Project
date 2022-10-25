3612127b4892a28618374f2c60008b29bbcac332
Theo Fandrich <thelogfan42@gmail.com>
2022-10-24 19:07:12 -0700

    setting up frontend

---
+++ react-app/src/components/LoginPage/index.js
@@ -0,0 +1,13 @@
+import React, { useEffect } from "react";
+import LoginForm from "./LoginForm";
+import SignUpForm from "./SignUpForm";
+
+const LoginPage = () => {
+  return (
+    <main>
+      <LoginForm />
+    </main>
+  );
+};
+
+export default LoginForm;
