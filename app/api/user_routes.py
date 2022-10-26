17978e77178a7e3cd936b4b024997e2416645cda
tyler <dtctorrero@icloud.com>
2022-10-25 21:49:10 -0700

    stuck on error handling on delete user

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -1,9 +1,19 @@
 from crypt import methods
 from flask import Blueprint, jsonify
-from flask_login import login_required, current_user
+from flask_login import login_required, current_user, logout_user
 from app.models import User, db
 
 user_routes = Blueprint('users', __name__)
+
+def validation_errors_to_error_messages(validation_errors):
+    """
+    Simple function that turns the WTForms validation errors into a simple list
+    """
+    errorMessages = []
+    for field in validation_errors:
+        for error in validation_errors[field]:
+            errorMessages.append(f'{field} : {error}')
+    return errorMessages
 
 
 @user_routes.route('/')
@@ -24,7 +34,13 @@ def user(id):
 @login_required
 def delete_user(id):
     user = User.query.get(id)
+    # try:
     if current_user.is_authenticated and current_user.id == user.id:
+        logout_user()
         db.session.delete(user)
         db.session.commit()
         return user.to_dict()
+    #----------------------------need to fix error handling for when you delete a given user -------------------------------------------------------
+    # except:
+    #     error = Exception('You are not this user')
+    #     return {'errors': error}, 401
