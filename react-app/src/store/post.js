d5d3903c4662ae3a348fb15af55334ce48ed43ab
Theo Fandrich <thelogfan42@gmail.com>
2022-10-31 20:10:10 -0700

    commit everything

--- react-app/src/store/post.js
+++ react-app/src/store/post.js
@@ -1,23 +1,22 @@
-const GET_POSTS = 'posts/GET_POSTS'
-
+const GET_POSTS = "posts/GET_POSTS";
 
 const getPosts = (posts) => ({
-    type: GET_POSTS,
-    payload: posts
-})
+  type: GET_POSTS,
+  payload: posts,
+});
 
-const initialState = { posts : null };
+const initialState = { posts: null };
 
 export const getAllPosts = () => async (dispatch) => {
-    const response = await fetch('/api/posts');
-    return response
-}
+  const response = await fetch("/api/posts");
+  return response;
+};
 
 export default function reducer(state = initialState, action) {
-    switch(action.type) {
-        case GET_POSTS:
-            return { posts: action.payload}
-        default:
-            return state
-    }
+  switch (action.type) {
+    case GET_POSTS:
+      return { posts: action.payload };
+    default:
+      return state;
+  }
 }
