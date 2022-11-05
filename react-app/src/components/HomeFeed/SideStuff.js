050ab0483d11098291beb7e6a68d51ae6ab4aebf
Theo Fandrich <thelogfan42@gmail.com>
2022-11-04 17:32:12 -0700

    added all styling except for add post and delete post need to be styled

--- react-app/src/components/HomeFeed/SideStuff.js
+++ react-app/src/components/HomeFeed/SideStuff.js
@@ -3,10 +3,8 @@ import { NavLink } from "react-router-dom";
 // import pic from "../../images/screenshot1-2x.png";
 import { useSelector } from "react-redux";
 
-
 const SideStuff = () => {
-
-  const user = useSelector((state) => state.session.user)
+  const user = useSelector((state) => state.session.user);
 
   return (
     <div className="side-stuff-container">
@@ -15,14 +13,33 @@ const SideStuff = () => {
         <div className="side-stuff-details-container">
           <div className="username-full-name-side-stuff">
             <strong>{user.username}</strong>
-            <span className="secondary-gray-text">{user.firstName} {user.lastName}</span>
+            <span className="secondary-gray-text">
+              {user.firstName} {user.lastName}
+            </span>
           </div>
         </div>
       </NavLink>
-      <div className="side-stuff-second-container"></div>
-      <span className="suggestions-for-you-styling secondary-gray-text">
-        Suggestions For You
-      </span>
+      <div className="side-stuff-second-container">
+        <div className="suggestions-for-you-styling secondary-gray-text">
+          © 2022 Finstagram Built by{" "}
+          <a
+            classname="about-links-styling"
+            href="https://www.linkedin.com/in/derek-torrero-02823018a/"
+          >
+            Derek Torrero
+          </a>
+          {", "}
+          <a href="https://www.linkedin.com/in/theofandrich/">Theo Fandrich</a>
+          {", and"}
+          <a
+            classname="about-links-styling"
+            href="https://www.linkedin.com/in/theofandrich/"
+          >
+            {" "}
+            Alan Deleon
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
