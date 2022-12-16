0f31c6ef065bff196e3344647e9db45698689b91
tyler <dtctorrero@icloud.com>
2022-12-16 12:21:41 -0800

    fix

--- app/models/posts.py
+++ app/models/posts.py
@@ -5,6 +5,9 @@ from datetime import datetime
 class Post(db.Model):
     __tablename__ = 'posts'
 
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
     likes = db.Column(db.Integer, default=0)
