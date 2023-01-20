e014aa049a463dec2da4ade1725a098e61cc9ff3
tyler <dtctorrero@icloud.com>
2023-01-20 11:47:45 -0800

    added seed data found bug on post modal shows just users profile picture and not the comment photo

--- app/seeds/posts.py
+++ app/seeds/posts.py
@@ -133,6 +133,181 @@ def seed_posts():
       caption='recon dart for A hell',
       likes=2
     )
+    # ----------------------------------------------------------------------------------
+    post22 = Post(
+      user_id=10,
+      img_url='https://capstone-strava-clone-aktiv.s3.us-west-2.amazonaws.com/Instagram+Seed+post+photos/1.jpg',
+      caption='If I dont like something, Ill stay away from it.',
+      likes=4
+    )
+    post23 = Post(
+      user_id=10,
+      img_url='https://capstone-strava-clone-aktiv.s3.us-west-2.amazonaws.com/Instagram+Seed+post+photos/10.jpg',
+      caption='He figured a few sticks of dynamite were easier than a fishing pole to catch fish.',
+      likes=3
+    )
+    post24 = Post(
+      user_id=1,
+      img_url='https://capstone-strava-clone-aktiv.s3.us-west-2.amazonaws.com/Instagram+Seed+post+photos/11.jpg',
+      caption='The three-year-old girl ran down the beach as the kite flew behind her.',
+      likes=3
+    )
+    post25 = Post(
+      user_id=1,
+      img_url='https://capstone-strava-clone-aktiv.s3.us-west-2.amazonaws.com/Instagram+Seed+post+photos/12.jpg',
+      caption='The light in his life was actually a fire burning all around him.',
+      likes=5
+    )
+    post26 = Post(
+      user_id=2,
+      img_url='https://capstone-strava-clone-aktiv.s3.us-west-2.amazonaws.com/Instagram+Seed+post+photos/13.jpg',
+      caption='She was the type of girl who wanted to live in a pink house.',
+      likes=6
+    )
+    post27 = Post(
+      user_id=2,
+      img_url='https://capstone-strava-clone-aktiv.s3.us-west-2.amazonaws.com/Instagram+Seed+post+photos/14.jpg',
+      caption='He had decided to accept his fate of accepting his fate.',
+      likes=2
+    )
+    post28 = Post(
+      user_id=3,
+      img_url='https://capstone-strava-clone-aktiv.s3.us-west-2.amazonaws.com/Instagram+Seed+post+photos/15.jpg',
+      caption='Its important to remember to be aware of rampaging grizzly bears.',
+      likes=5
+    )
+    post29 = Post(
+      user_id=3,
+      img_url='https://capstone-strava-clone-aktiv.s3.us-west-2.amazonaws.com/Instagram+Seed+post+photos/16.jpg',
+      caption='There should have been a time and a place, but this wasnt it.',
+      likes=4
+    )
+    post30 = Post(
+      user_id=11,
+      img_url='https://capstone-strava-clone-aktiv.s3.us-west-2.amazonaws.com/Instagram+Seed+post+photos/17.jpg',
+      caption='The minute she landed she understood the reason this was a fly-over state.',
+      likes=5
+    )
+    post31 = Post(
+      user_id=11,
+      img_url='https://capstone-strava-clone-aktiv.s3.us-west-2.amazonaws.com/Instagram+Seed+post+photos/18.jpg',
+      caption='Two more days and all his problems would be solved.',
+      likes=3
+    )
+    post32 = Post(
+      user_id=11,
+      img_url='https://capstone-strava-clone-aktiv.s3.us-west-2.amazonaws.com/Instagram+Seed+post+photos/19.jpg',
+      caption='Purple is the best city in the forest.',
+      likes=7
+    )
+    post33 = Post(
+      user_id=12,
+      img_url='https://capstone-strava-clone-aktiv.s3.us-west-2.amazonaws.com/Instagram+Seed+post+photos/2.jpg',
+      caption='She used her own hair in the soup to give it more flavor.',
+      likes=5
+    )
+    post34 = Post(
+      user_id=12,
+      img_url='https://capstone-strava-clone-aktiv.s3.us-west-2.amazonaws.com/Instagram+Seed+post+photos/20.jpg',
+      caption='Id always thought lightning was something only I could see.',
+      likes=5
+    )
+    post35 = Post(
+      user_id=12,
+      img_url='https://capstone-strava-clone-aktiv.s3.us-west-2.amazonaws.com/Instagram+Seed+post+photos/21.jpg',
+      caption='You cant compare apples and oranges, but what about bananas and plantains?',
+      likes=4
+    )
+    post36 = Post(
+      user_id=13,
+      img_url='https://capstone-strava-clone-aktiv.s3.us-west-2.amazonaws.com/Instagram+Seed+post+photos/22.jpg',
+      caption='The best key lime pie is still up for debate.',
+      likes=2
+    )
+    post37 = Post(
+      user_id=13,
+      img_url='https://capstone-strava-clone-aktiv.s3.us-west-2.amazonaws.com/Instagram+Seed+post+photos/23.jpg',
+      caption='I love bacon, beer, birds, and baboons.',
+      likes=2
+    )
+    post38 = Post(
+      user_id=13,
+      img_url='https://capstone-strava-clone-aktiv.s3.us-west-2.amazonaws.com/Instagram+Seed+post+photos/24.jpg',
+      caption='The beauty of the sunset was obscured by the industrial cranes.',
+      likes=1
+    )
+    post39 = Post(
+      user_id=14,
+      img_url='https://capstone-strava-clone-aktiv.s3.us-west-2.amazonaws.com/Instagram+Seed+post+photos/25.jpg',
+      caption='She did a happy dance because all of the socks from the dryer matched.',
+      likes=5
+    )
+    post40 = Post(
+      user_id=14,
+      img_url='https://capstone-strava-clone-aktiv.s3.us-west-2.amazonaws.com/Instagram+Seed+post+photos/26.jpg',
+      caption='The thick foliage and intertwined vines made the hike nearly impossible.',
+      likes=6
+    )
+    post41 = Post(
+      user_id=14,
+      img_url='https://capstone-strava-clone-aktiv.s3.us-west-2.amazonaws.com/Instagram+Seed+post+photos/27.jpg',
+      caption='That must be the tenth time Ive been arrested for selling deep-fried cigars.',
+      likes=4
+    )
+    post42 = Post(
+      user_id=15,
+      img_url='https://capstone-strava-clone-aktiv.s3.us-west-2.amazonaws.com/Instagram+Seed+post+photos/28.jpg',
+      caption='Jerry liked to look at paintings while eating garlic ice cream.',
+      likes=5
+    )
+    post43 = Post(
+      user_id=15,
+      img_url='https://capstone-strava-clone-aktiv.s3.us-west-2.amazonaws.com/Instagram+Seed+post+photos/29.jpg',
+      caption='He was sure the Devil created red sparkly glitter.',
+      likes=7
+    )
+    post44 = Post(
+      user_id=15,
+      img_url='https://capstone-strava-clone-aktiv.s3.us-west-2.amazonaws.com/Instagram+Seed+post+photos/3.jpg',
+      caption='The childs favorite Christmas gift was the large box her fathers lawnmower came in.',
+      likes=5
+    )
+    post45 = Post(
+      user_id=4,
+      img_url='https://capstone-strava-clone-aktiv.s3.us-west-2.amazonaws.com/Instagram+Seed+post+photos/4.jpg',
+      caption='He wondered if it could be called a beach if there was no sand.',
+      likes=2
+    )
+    post46 = Post(
+      user_id=1,
+      img_url='https://capstone-strava-clone-aktiv.s3.us-west-2.amazonaws.com/Instagram+Seed+post+photos/5.jpg',
+      caption='It had been sixteen days since the zombies first attacked.',
+      likes=2
+    )
+    post47 = Post(
+      user_id=1,
+      img_url='https://capstone-strava-clone-aktiv.s3.us-west-2.amazonaws.com/Instagram+Seed+post+photos/6.jpg',
+      caption='He had a vague sense that trees gave birth to dinosaurs.',
+      likes=2
+    )
+    post48 = Post(
+      user_id=2,
+      img_url='https://capstone-strava-clone-aktiv.s3.us-west-2.amazonaws.com/Instagram+Seed+post+photos/7.jpg',
+      caption='Peanut butter and jelly caused the elderly lady to think about her past.',
+      likes=4
+    )
+    post49 = Post(
+      user_id=5,
+      img_url='https://capstone-strava-clone-aktiv.s3.us-west-2.amazonaws.com/Instagram+Seed+post+photos/8.jpg',
+      caption='He never understood why what, when, and where left out who.',
+      likes=2
+    )
+    post50 = Post(
+      user_id=8,
+      img_url='https://capstone-strava-clone-aktiv.s3.us-west-2.amazonaws.com/Instagram+Seed+post+photos/9.jpg',
+      caption='He created a pig burger out of beef.',
+      likes=2
+    )
 
     db.session.add(post1)
     db.session.add(post2)
@@ -155,7 +330,37 @@ def seed_posts():
     db.session.add(post19)
     db.session.add(post20)
     db.session.add(post21)
-
+    db.session.add_all([
+      post22,
+      post23,
+      post24,
+      post25,
+      post26,
+      post27,
+      post28,
+      post29,
+      post30,
+      post31,
+      post32,
+      post33,
+      post34,
+      post35,
+      post36,
+      post37,
+      post38,
+      post39,
+      post40,
+      post41,
+      post42,
+      post43,
+      post44,
+      post45,
+      post46,
+      post47,
+      post48,
+      post49,
+      post50
+      ])
 
     db.session.commit()
 
--- app/seeds/users.py
+++ app/seeds/users.py
@@ -1,48 +1,64 @@
 from app.models import db, User
-from faker import Faker
+# from faker import Faker
 
-fake = Faker()
-user_data = []
+# fake = Faker()
+# user_data = []
 
-for i in range(16):
-    user_data.append(
-        {'first_name': fake.first_name(), 'last_name': fake.last_name(), 'password': fake.password(), 'username': fake.simple_profile()['username'], 'email': fake.free_email(), 'profile_picture': fake.image_url()}
-    )
-print(user_data)
+# for i in range(16):
+#     user_data.append(
+#         {'first_name': fake.first_name(), 'last_name': fake.last_name(), 'password': fake.password(), 'username': fake.simple_profile()['username'], 'email': fake.free_email(), 'profile_picture': fake.image_url()}
+#     )
+# print(user_data)
 
 # Adds a demo user, you can add other users here if you want
 def seed_users():
     demo = User( #1
-        username='Demo', email='demo@aa.io', password='password', first_name='demo', last_name='user')
+        username='Demo', email='demo@aa.io', password='password', first_name='demo', last_name='user', profile_picture='https://capstone-strava-clone-aktiv.s3.us-west-2.amazonaws.com/Instagram+Seed+Photos/1.jpg')
     marnie = User( #2
-        username='marnie', email='marnie@aa.io', password='password', first_name='demo1', last_name='user1')
+        username='marnie', email='marnie@aa.io', password='password', first_name='demo1', last_name='user1', profile_picture='https://capstone-strava-clone-aktiv.s3.us-west-2.amazonaws.com/Instagram+Seed+Photos/10.jpg')
     bobbie = User( #3
-        username='bobbie', email='bobbie@aa.io', password='password', first_name='demo2', last_name='user2')
+        username='bobbie', email='bobbie@aa.io', password='password', first_name='demo2', last_name='user2', profile_picture='https://capstone-strava-clone-aktiv.s3.us-west-2.amazonaws.com/Instagram+Seed+Photos/11.jpg')
     theo = User( #4
-        username='theoman42', email='theo@aa.io', password='password', first_name='theo', last_name='fan')
+        username='theoman42', email='theo@aa.io', password='password', first_name='theo', last_name='fan', profile_picture='https://capstone-strava-clone-aktiv.s3.us-west-2.amazonaws.com/Instagram+Seed+Photos/12.jpg')
     derek = User( #5
-        username='torman', email='tor@aa.io', password='password', first_name='derk', last_name='tor')
+        username='torman', email='tor@aa.io', password='password', first_name='derk', last_name='tor', profile_picture='https://capstone-strava-clone-aktiv.s3.us-west-2.amazonaws.com/Instagram+Seed+Photos/13.jpg')
     alan = User( #6
-        username='awdel88', email='awd@aa.io', password='password', first_name='alan', last_name='del')
+        username='awdel88', email='awd@aa.io', password='password', first_name='alan', last_name='del', profile_picture='https://capstone-strava-clone-aktiv.s3.us-west-2.amazonaws.com/Instagram+Seed+Photos/14.jpg')
     gabe = User( #7
-        username='gabeknight', email='knight@aa.io', password='password', first_name='gabriel', last_name='knight')
+        username='gabeknight', email='knight@aa.io', password='password', first_name='gabriel', last_name='knight', profile_picture='https://capstone-strava-clone-aktiv.s3.us-west-2.amazonaws.com/Instagram+Seed+Photos/15.jpg')
     grace = User( #8
-        username='grace2', email='chang@aa.io', password='password', first_name='grace', last_name='wong')
+        username='grace2', email='chang@aa.io', password='password', first_name='grace', last_name='wong', profile_picture='https://capstone-strava-clone-aktiv.s3.us-west-2.amazonaws.com/Instagram+Seed+Photos/2.jpg')
     wilson = User( #9
-        username='lalawill', email='healz@aa.io', password='password', first_name='wilson', last_name='lee')
+        username='lalawill', email='healz@aa.io', password='password', first_name='wilson', last_name='lee', profile_picture='https://capstone-strava-clone-aktiv.s3.us-west-2.amazonaws.com/Instagram+Seed+Photos/3.jpg')
     chris = User( #10
-        username='chrisTheMan', email='tofu@aa.io', password='password', first_name='chris', last_name='pork')
+        username='chrisTheMan', email='tofu@aa.io', password='password', first_name='chris', last_name='pork', profile_picture='https://capstone-strava-clone-aktiv.s3.us-west-2.amazonaws.com/Instagram+Seed+Photos/4.jpg')
+        # //----------------------------
+    matt = User( #11
+        username='MattyIce', email='mattyice@aa.io', password='password', first_name='matt', last_name='smith', profile_picture='https://capstone-strava-clone-aktiv.s3.us-west-2.amazonaws.com/Instagram+Seed+Photos/5.jpg')
+    taylor = User( #12
+        username='taytay', email='taytay@aa.io', password='password', first_name='taylor', last_name='armstrong', profile_picture='https://capstone-strava-clone-aktiv.s3.us-west-2.amazonaws.com/Instagram+Seed+Photos/6.jpg')
+    jane = User( #13
+        username='janedoe', email='janedoe@aa.io', password='password', first_name='jane', last_name='doe', profile_picture='https://capstone-strava-clone-aktiv.s3.us-west-2.amazonaws.com/Instagram+Seed+Photos/7.jpg')
+    danielle = User( #14
+        username='itsdanelle', email='danelle@aa.io', password='password', first_name='danielle', last_name='schrader', profile_picture='https://capstone-strava-clone-aktiv.s3.us-west-2.amazonaws.com/Instagram+Seed+Photos/8.jpg')
+    ronald = User( #15
+        username='ronweasley', email='weasley@aa.io', password='password', first_name='ronald', last_name='weasley', profile_picture='https://capstone-strava-clone-aktiv.s3.us-west-2.amazonaws.com/Instagram+Seed+Photos/9.jpg')
 
 
 
-    demo.followers = [marnie, bobbie, alan, theo, derek, chris]
+    demo.followers = [marnie, bobbie, alan, theo, derek, chris, ronald]
     marnie.followers = [bobbie, demo, alan, gabe, wilson]
     bobbie.followers = [marnie, demo, grace, theo, derek, alan, chris]
-    theo.followers = [alan, derek, gabe]
-    derek.followers = [theo, alan, bobbie]
-    gabe.followers = [alan, chris]
-    grace.followers = [chris, bobbie, marnie]
-    chris.followers = [alan]
+    theo.followers = [alan, derek, gabe, ronald]
+    derek.followers = [theo, alan, bobbie, danielle]
+    gabe.followers = [alan, chris, demo, marnie, jane]
+    grace.followers = [chris, bobbie, marnie, taylor]
+    chris.followers = [alan, gabe, bobbie, jane]
+    matt.followers = [demo, gabe, chris, taylor]
+    taylor.followers = [jane, ronald, bobbie, grace]
+    jane.followers = [chris, danielle, derek, bobbie]
+    danielle.followers = [matt, theo, demo, ronald]
+    ronald.followers = [matt, alan, marnie, taylor, danielle]
 
 
     db.session.add(demo)
@@ -55,6 +71,14 @@ def seed_users():
     db.session.add(grace)
     db.session.add(wilson)
     db.session.add(chris)
+    db.session.add(matt)
+    db.session.add(taylor)
+    db.session.add(jane)
+    db.session.add(danielle)
+    db.session.add(ronald)
+
+
+
 
     db.session.commit()
 
